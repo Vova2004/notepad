4e5eaaa64428f99d5a082463fbaa862601580369
ukrasket <ukrasket@gmail.com>
2019-09-22 22:22:27 +0300

    Notepad v.1, working

--- post.rb
+++ post.rb
@@ -1,4 +1,12 @@
 class Post
+ def self.post_types
+   [Memo, Link, Task]
+ end
+
+ def self.create (type_index)
+   return post_types[type_index].new
+ end
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -21,7 +29,7 @@ class Post
   end
 
   def file_path
-    current_path = File.dirname(__FILE__ __)
+    current_path = File.dirname(__FILE__)
     file_name = @created_at.strftime("#{self.class.name}_%Y-%m-%d-%H-%M-%S.txt")
     return current_path + "/" + file_name
   end
